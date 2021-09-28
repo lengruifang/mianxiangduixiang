1831ad20e257975da7ce07838570b2fe95275cc1
陈俊生 <2810955497@qq.com>
2021-09-28 22:04:49 +0800

    可以全部通过测试，但有些部分比较粗糙，需要优化。

--- src/main/java/practice11/Person.java
+++ src/main/java/practice11/Person.java
@@ -1,4 +1,37 @@
 package practice11;
 
+import java.util.Objects;
+
 public class Person {
+    String Name;
+    int Age;
+    int id;
+    public Person(int id,String Name,int Age){
+        this.id=id;
+        this.Name=Name;
+        this.Age=Age;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Age == person.Age &&
+                id == person.id &&
+                Name.equals(person.Name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Name, Age, id);
+    }
+    public  String getName(){
+        return Name;
+    }
+    public int getAge(){
+        return Age;
+    }
+    public String introduce() {
+        return "My name is "+Name+". I am "+Age+" years old.";
+    }
 }
