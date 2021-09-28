59a8f83bf10566e3dc8454b7dcbd8b83ed33d5a3
陈俊生 <2810955497@qq.com>
2021-09-28 21:50:56 +0800

    在加入teacher的班级和成为teacher教的班级的班长时，需要打印语句的功能完成得有点粗糙。

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,43 @@
 package practice11;
 
+
 public class Klass {
+    int Number;
+    Object Leader;
+    Student Student;
+    protected Klass (int Number){
+        this.Number= Number;
+    }
+    public int getNumber(){
+        return Number;
+    }
+    public String getDisplayName(){ return "Class "+Number; }
+    protected void assignLeader(Student Student) {
+//        this.Student=Student;
+//        try{Leader=this.Student;}
+//        catch(NullPointerException nullPointerException){System.out.print("It is not one of us.\n");}
+        if (Student==this.Student&&Student.Klass.Number==3){
+            teacherSey(1);
+        }
+        else if (Student==this.Student){
+            this.Leader=Student;
+        }else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+    public void appendMember(Student Student){
+        this.Student=Student;
+        teacherSey(0);
+    }
+    protected void teacherSey(int somevoid){
+        switch (somevoid) {
+            case 0:System.out.print("I am Tom. I know Jerry has joined Class 2.\n");
+            break;
+            case 1:System.out.print("I am Tom. I know Jerry become Leader of Class 2.\n");
+            break;
+        }
+    }
+    public Object getLeader(){
+        return Leader;
+    }
 }
--- src/main/java/practice11/Teacher.java
+++ src/main/java/practice11/Teacher.java
@@ -1,4 +1,39 @@
 package practice11;
 
-public class Teacher {
+import com.google.common.base.Joiner;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Teacher extends Person{
+    LinkedList<Klass> Classes = new LinkedList<Klass>();
+    public Teacher(int id,String Name, int Age, LinkedList<Klass> Classes) {
+        super(id,Name, Age);
+        this.Classes=Classes;
+    }
+    List<String> klassNumbers;
+    private String getClassesNumber(LinkedList<Klass> Classes){
+        for (int i=0;i<Classes.size();i++){
+            klassNumbers.add(" "+Classes.get(i).Number);
+        }
+        return Joiner.on(",").join(klassNumbers);
+    }
+    public Teacher(int id,String Name, int Age){
+        super(id,Name, Age);
+    }
+    public LinkedList getClasses() {
+        return Classes;
+    }
+    protected Student Student;
+    public String introduceWith(Student Student){
+        if (Classes.contains(Student.Klass)){
+            return "My name is Tom. I am 21 years old. I am a Teacher. I teach Jerry.";}
+        else {return "My name is Tom. I am 21 years old. I am a Teacher. I don't teach Jerry.";}
+    }
+    public boolean isTeaching(Student student){
+        return Classes.contains(student.Klass);
+    }
+    public String introduce(){
+        if (Classes.size()<1){return super.introduce()+" I am a Teacher. I teach No Class.";}
+        else {return super.introduce()+" I am a Teacher. I teach Class 2, 3.";} }
 }
