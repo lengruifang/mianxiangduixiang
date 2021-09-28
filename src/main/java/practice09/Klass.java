a40f9fb7f253ba3d32b6ef0f84f254d8b36847cb
陈俊生 <2810955497@qq.com>
2021-09-28 10:54:07 +0800

    不会assign成功时的情况试图使用try catch完成没有成功，使用了if else语句。

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -3,6 +3,7 @@ package practice09;
 public class Klass {
     int Number;
     Object Leader;
+    Student Student;
     protected Klass (int Number){
         this.Number= Number;
     }
@@ -10,9 +11,15 @@ public class Klass {
         return Number;
     }
     public String getDisplayName(){ return "Class "+Number; }
-    protected Object assignLeader(Student Student) {
-        try{if (this.Student!=null)return Leader = Student;}
-        catch(IllegalArgumentException){System.out.print("1");}
+    protected void assignLeader(Student Student) {
+//        this.Student=Student;
+//        try{Leader=this.Student;}
+//        catch(NullPointerException nullPointerException){System.out.print("It is not one of us.");}
+        if (Student==this.Student){
+            this.Leader=Student;
+        }else {
+            System.out.print("It is not one of us.\n");
+        }
     }
     public Object appendMember(Student Student){
         this.Student=Student;
@@ -21,5 +28,4 @@ public class Klass {
     public Object getLeader(){
         return Leader;
     }
-    Student Student;
 }
