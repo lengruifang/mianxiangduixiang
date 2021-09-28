6d8bfd5704b4ddbbad20afc4b1f309a2c270566c
陈俊生 <2810955497@qq.com>
2021-09-28 11:25:12 +0800

    对为何要重写equals方法进行分析，写上注释。

--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -20,12 +20,13 @@ public class Person {
                 id == person.id &&
                 Name.equals(person.Name);
     }
-
     @Override
     public int hashCode() {
         return Objects.hash(Name, Age, id);
     }
-
+//==是java提供的等于比较运算符,用来比较两个变量指向的内存地址是否相同.而equals()是Object提供的一个方法
+//.Object中equals()方法的默认实现就是返回两个对象==的比较结果.但是equals()可以被重写，而重写equals()必须也要重写hashCode()
+//因为，在java中，对对象的存储采取了存储在哈希表中处理方法，hashcode方法是根据对象的地址转换之后返回的一个哈希值
     public  String getName(){
         return Name;
     }
